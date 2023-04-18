e2c0c3eae10b373ea9c8cbadac199e0ce34014dc
MateuszWeclawowicz <S00224403@mail.itsligo.ie>
2023-04-18 19:57:41 +0100

    fixed index.mjs

--- index.mjs
+++ index.mjs
@@ -8,6 +8,17 @@ const PORT = process.env.PORT || 5050;
 const app = express();
 
 app.use(cors());
+
+// Set specific CORS headers if needed
+app.use((req, res, next) => {
+  //res.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
+  res.setHeader("Access-Control-Allow-Origin", "http://carsales-nosql.s3-website-eu-west-1.amazonaws.com"); // Set the allowed origin
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS"); // Set the allowed HTTP methods
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization"); // Set the allowed headers
+  res.setHeader("Access-Control-Allow-Credentials", "true"); // Set the credentials flag to true
+  next();
+});
+
 app.use(express.json());
 
 // Load the /posts routes
