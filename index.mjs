3b1202e98cf5ebb6aebc8898e3c2950c25166944
MateuszWeclawowicz <S00224403@mail.itsligo.ie>
2023-04-18 20:35:25 +0100

    updated index.mjs

--- index.mjs
+++ index.mjs
@@ -7,17 +7,15 @@ import posts from "./routes/posts.mjs";
 const PORT = process.env.PORT || 5050;
 const app = express();
 
-app.use(cors());
+// Set CORS options
+const corsOptions = {
+  origin: "http://carsales-nosql.s3-website-eu-west-1.amazonaws.com", // Set the allowed origin
+  methods: "GET, POST, PUT, DELETE, OPTIONS", // Set the allowed HTTP methods
+  allowedHeaders: "Content-Type, Authorization", // Set the allowed headers
+  credentials: true // Set the credentials flag to true
+};
 
-// Set specific CORS headers if needed
-app.use((req, res, next) => {
-  //res.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
-  res.setHeader("Access-Control-Allow-Origin", "http://carsales-nosql.s3-website-eu-west-1.amazonaws.com"); // Set the allowed origin
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS"); // Set the allowed HTTP methods
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization"); // Set the allowed headers
-  res.setHeader("Access-Control-Allow-Credentials", "true"); // Set the credentials flag to true
-  next();
-});
+app.use(cors(corsOptions));
 
 app.use(express.json());
 
@@ -26,8 +24,8 @@ app.use("/cars", posts);
 
 // Global error handling
 app.use((err, _req, res, next) => {
-  res.status(500).send("Uh oh! An unexpected error occured.")
-})
+  res.status(500).send("Uh oh! An unexpected error occured.");
+});
 
 // start the Express server
 app.listen(PORT, () => {
